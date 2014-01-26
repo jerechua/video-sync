239c59b28132209bce65ea2f0aba3a1a7f264d1c
Jeremy <jere.chua@gmail.com>
2014-01-26 00:29:34 -0800

    syncs and can change video file

--- socket.js
+++ socket.js
@@ -1,25 +1,35 @@
 exports.setupSocket = function(server) {
 
-  var io = require('socket.io').listen(server);
-  //set log level to warn
-  io.set('log level', 1);
-
-  io.sockets.on('connection', function (socket) {
-    socket.on('player:pause', function(data) {
-        socket.broadcast.emit('player:pause', data);
-    });
-
-    socket.on('player:play', function(data) {
-        socket.broadcast.emit('player:play', data);
-    });
-
-    socket.on('player:timeupdate', function(data) {
-        // Implement this for slow internet connection buffers
-    });
-
-    socket.on('disconnect', function() {
-        //clean up
-    });
+    var currentVideo = "";
     
-  });
+    var io = require('socket.io').listen(server);
+    //set log level to warn
+    io.set('log level', 1);
+
+    io.sockets.on('connection', function (socket) {
+        if (currentVideo != "") {
+            socket.emit('startup:current_video', {'src': currentVideo});
+        }
+        socket.on('player:pause', function(data) {
+            socket.broadcast.emit('player:pause', data);
+        });
+
+        socket.on('player:play', function(data) {
+            socket.broadcast.emit('player:play', data);
+        });
+
+        socket.on('player:timeupdate', function(data) {
+            // Implement this for slow internet connection buffers
+        });
+
+        socket.on('player:change_src', function(data) {
+            currentVideo = data['src'];
+            socket.broadcast.emit('player:change_src', data);
+        });
+
+        socket.on('disconnect', function() {
+            //clean up
+        });
+
+    });
 }
