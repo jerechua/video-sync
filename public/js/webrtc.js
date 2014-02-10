69d98946c740f870cb6d06c046f295b8d5dc7603
Jeremy <jere.chua@gmail.com>
2014-02-09 16:06:59 -0800

    That was easy

--- public/js/webrtc.js
+++ public/js/webrtc.js
@@ -1,3 +1,17 @@
-$(function() {
+function initialize() {
+  var webrtc = new SimpleWebRTC({
+    localVideoEl: 'my-webcam',
+    remoteVideosEl: 'friends-webcams',
+    autoRequestMedia: true
+  });
 
+  return webrtc;
+}
+
+$(function() {
+  var webrtc = initialize();
+
+  webrtc.on('readyToCall', function() {
+    webrtc.joinRoom(roomName);
+  });
 });
